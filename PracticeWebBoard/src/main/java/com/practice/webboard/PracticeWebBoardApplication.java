8019f631c199f043dbee0101a4be258d8fc187a8
Yang tae wan <solme47@gmail.com>
2023-08-17 17:02:06 +0900

    0.0.1
    start from here

---
+++ PracticeWebBoard/src/main/java/com/practice/webboard/PracticeWebBoardApplication.java
@@ -0,0 +1,13 @@
+package com.practice.webboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeWebBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeWebBoardApplication.class, args);
+	}
+
+}
---
+++ PracticeWebBoard/src/test/java/com/practice/webboard/PracticeWebBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.webboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeWebBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
